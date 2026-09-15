f61c549f9ec61ac8a0c05b51ed7e5777b73edbc9
Corentin Leandre <corentinleandre+github@gmail.com>
2026-09-16 00:56:40 +0200

    Added BaseCreature to registry

--- src/module/data/actor/_module.mjs
+++ src/module/data/actor/_module.mjs
@@ -1,9 +1,11 @@
 import ChessModel from "./chess.mjs";
 import GameTokenModel from "./game-token.mjs";
+import BaseCreature from "./base-creature.mjs";
 
 const config = {
   chess: ChessModel,
   token: GameTokenModel,
+  basecreature: BaseCreature,
 };
 
-export { ChessModel, GameTokenModel, config };
+export { ChessModel, GameTokenModel, BaseCreature, config };
