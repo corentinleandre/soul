279f1674d9f82deab0a2a9de7c57417ba4632049
Corentin Leandre <corentinleandre+github@gmail.com>
2026-09-15 20:49:25 +0200

    Adding stats to Actor header

--- src/module/documents/SOULActor.mjs
+++ src/module/documents/SOULActor.mjs
@@ -22,6 +22,15 @@ export default class SOULActor extends foundry.documents.Actor {
   prepareDerivedData() {
     super.prepareDerivedData();
 
+    this.strengthBonus = Math.floor(this.strength/10)
+    this.dexterityBonus = Math.floor(this.dexterity/10)
+    this.sensesBonus = Math.floor(this.senses/10)
+    this.enduranceBonus = Math.floor(this.endurance/10)
+    this.agilityBonus = Math.floor(this.agility/10)
+    this.intelligenceBonus = Math.floor(this.intelligence/10)
+    this.resilienceBonus = Math.floor(this.resilience/10)
+    this.socialBonus = Math.floor(this.social/10)
+
     /**
      * Flexible hook for modules to alter derived document data.
      * @param {SOULActor} actor      The actor preparing derived data.
