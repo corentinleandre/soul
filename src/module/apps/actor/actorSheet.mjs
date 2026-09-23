ded43c1768fc6dc9000377851670ab1697359bda
Corentin Leandre <corentinleandre+github@gmail.com>
2026-09-23 02:49:13 +0200

    Added debugging

--- src/module/apps/actor/actorSheet.mjs
+++ src/module/apps/actor/actorSheet.mjs
@@ -86,7 +86,7 @@ export class SOULActorSheet extends api.HandlebarsApplicationMixin(sheets.ActorS
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
 
-    console.log(this.actor.system);
+    console.log(this.actor.system.schema.fields);
 
     Object.assign(context, {
       owner: this.document.isOwner,
