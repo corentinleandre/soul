a3edb0b71cac8b5b1c4b9746ff1f247b043dc4af
Corentin Leandre <corentinleandre+github@gmail.com>
2026-09-25 19:06:48 +0200

    Updated Model

--- src/module/data/actor/base.mjs
+++ src/module/data/actor/base.mjs
@@ -1,12 +1,13 @@
 import ActorDataModel from "../abstract/actor-data-model.mjs"
-import { StatsModel } from "./components/stats"
+import { StatsModel } from "./components/stats.mjs"
+import { CharacteristicsModel } from "./components/characteristics.mjs"
 
 const { SchemaField, NumberField } = foundry.data.fields;
 
 /**
  * Simple data model for base creatures as a type of actor.
  */
-export default class BaseModel extends ActorDataModel.mixin(StatsModel) {
+export default class BaseModel extends ActorDataModel.mixin(StatsModel, CharacteristicsModel) {
   /** @inheritdoc */
   static LOCALIZATION_PREFIXES = ["SOUL.Base"];
 
